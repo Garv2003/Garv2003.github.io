3c437d41af6dddb55d2372143e5707d79be13378
garv aggarwal <aggarwalgarv0505@gmail.com>
2026-07-10 16:50:27 +0530

    Add favicon, résumé download, /systems nav, sitemap, robots, 404, JSON-LD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build
 export default defineConfig({
   site: 'https://garv2003.github.io',
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
